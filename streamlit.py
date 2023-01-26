c52a788904dadb9e6709e8ba833095e9a8882611
ayoubamine1 <96729428+ayoubamine1@users.noreply.github.com>
2023-01-26 21:16:30 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,55 +1,44 @@
-
-
 import streamlit as st
 import pickle
-import sqlite3
-import random
-import time 
+import numpy as np
 
-f = open('word_list.js')
-stg = f.read()
-stg = stg.replace('\n','').replace(';','').split('=')[1].replace('[','').replace(']','').replace('"','').replace(' ','')
-word =str(stg).split(',')
-
-
-def select_unique_random_words(words, num_words):
-    # Connect to the SQLite database
-    conn = sqlite3.connect('selected_words.db')
-    c = conn.cursor()
-    # Create the table if it doesn't exist
-    c.execute('''CREATE TABLE IF NOT EXISTS selected_words
-                (words text)''')
-    size =  print(c.execute("SELECT COUNT(*) FROM selected_words").fetchone())
-    selected_words = random.sample(words, num_words)
-    while True:
-        # Check if the selected words are already in the table
-        c.execute("SELECT * FROM selected_words WHERE words = ?", (str(selected_words),))
-        if c.fetchone() is None:
-            # if not in table, insert the selected words and return
-            c.execute("INSERT INTO selected_words (words) VALUES (?)", (str(selected_words),))
-            conn.commit()
-            return selected_words,size
-        else:
-            # if already in table, reselect words
-            selected_words = random.sample(words, num_words),size
-    conn.close()
-
-
-
-#feel = st.text_input("Enter the Feeling: ",key="feeling")
 
 
 if st.button('Generate Words'):
-    
-    for i in range(100000):  
-        selected_words,size = select_unique_random_words(word, 12)
-        sentence =  ', '.join(selected_words)
-        st.write(f"The words are : {sentence}{ size}")
+    file = open('state.txt')
+    state = []
+    for line in file:
+        state.append(int(line.replace('\n','')))
+    file.close()
 
-    
+    # extract the words from the file 
+    f = open('word_list.js')
+    stg = f.read()
+    stg = stg.replace('\n','').replace(';','').split('=')[1].replace('[','').replace(']','').replace('"','').replace(' ','')
+    words =str(stg).split(',')
+    f.close()
 
+    n = len(words)
 
+    i = 0
+    while i <=10000 :
+        if len(np.unique(state)) == 12 : 
+            lst =  [words[j] for j in state]
+            sentence = ' '.join(lst)
+            st.write(f"The words are : {sentence}")
+            i+=1
 
-
-
-
+        state[0] += 1
+        for k in range(11):
+            if state [k] == n :
+                state[k] = 0
+                state[k+1] += 1
+            else: 
+                break 
+        if state[-1] == n:
+            state = [12-i for i in range(1,13)]
+    file = open('state.txt','w')
+    for i in state:
+        file.write(str(i)+'\n')
+    file.close()
+            
